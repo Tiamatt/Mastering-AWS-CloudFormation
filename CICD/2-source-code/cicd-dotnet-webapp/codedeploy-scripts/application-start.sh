c0824b17aa99b2a48429baf68ce9577b41fc54cb
tiamatt <samira.yusifova@gmail.com>
2021-02-09 01:12:39 -0500

    Update CICD/2-source-code

--- CICD/2-source-code/cicd-dotnet-webapp/codedeploy-scripts/application-start.sh
+++ CICD/2-source-code/cicd-dotnet-webapp/codedeploy-scripts/application-start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo kill $(ps aux | grep 'cicd-dotnet-webapp.dll' | awk '{print $2}')
 screen -d -m -S SERVER bash -c 'cd ../home/ec2-user/cicd-dotnet-webapp && sudo dotnet cicd-dotnet-webapp.dll'
